57f9cea779a74521c0143a77e874ffca7981a49b
YAMEOGO Wendemi Francine A Marie Joseph <yameogofrancine6@gmail.com>
2026-01-28 09:29:03 +0000

    update

--- Backend/src/routes/cartRoutes.js
+++ Backend/src/routes/cartRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
+const { authenticate } = require('../middleware/authMiddleware');
 const { validate } = require('../validators/authValidators');
 const { addItemSchema, updateItemSchema } = require('../validators/salesValidators');
 
