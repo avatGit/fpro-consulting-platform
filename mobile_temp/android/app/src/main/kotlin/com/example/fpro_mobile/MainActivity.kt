2e2c431f5156c2f9e497a34249ef46d1aca0fa52
Giscard001 <ccjdicus@gmail.com>
2026-03-09 18:37:52 +0000

    Suppression de doublons hors de mobile_frontend-fpro

--- mobile_temp/android/app/src/main/kotlin/com/example/fpro_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fpro_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile_temp/lib/core/constants/storage_keys.dart
+++
@@ -1,8 +0,0 @@
-class StorageKeys {
-  static const String accessToken = 'access_token';
-  static const String refreshToken = 'refresh_token';
-  static const String userId = 'user_id';
-  static const String userRole = 'user_role';
-  static const String userEmail = 'user_email';
-  static const String isLoggedIn = 'is_logged_in';
-}
